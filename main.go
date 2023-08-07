e22cb55abadc076aed5f5f4e7cf140c8a40c21a7
dexxxter325 <107575449+dexxxter325@users.noreply.github.com>
2023-08-07 18:50:22 +0300

    main.go

--- main.go
+++ main.go
@@ -14,13 +14,25 @@ const (
 	openWeatherMapAPI = "e71cc7509ba7040322d574ebdad1b5c3"
 )
 
-func GetWeather(city string, openWeatherMapAPI string) {
+type WeatherData struct {
+	City        string
+	Humidity    float64
+	Pressure    float64
+	WindSpeed   float64
+	Sunrise     time.Time
+	Sunset      time.Time
+	CurWeather  float64
+	wd          string
+	LengthOfDay time.Duration
+}
+
+func GetWeather(city string, openWeatherMapAPI string) WeatherData {
 	url := "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + openWeatherMapAPI + "&units=metric"
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Проверьте название города")
-		return
+
 	}
 	defer res.Body.Close()
 
@@ -32,37 +44,31 @@ func GetWeather(city string, openWeatherMapAPI string) {
 
 	fmt.Printf("%+v\n", data)
 
-	city = data["name"].(string)
 	weather := data["main"].(map[string]interface{})
-	curWeather := weather["temp"].(float64)
 	weatherDescription := data["weather"].([]interface{})[0].(map[string]interface{})["main"].(string)
-	var wd string
+	var _ string
 	if weatherDescription, ok := codeToSmile[weatherDescription]; ok {
-		wd = weatherDescription
+		_ = weatherDescription
 	} else {
-		wd = "Посмотри в окно, не пойму что там за погода!"
+		_ = "Посмотри в окно, не пойму что там за погода!"
 	}
-
-	humidity := weather["humidity"].(float64)
-	pressure := weather["pressure"].(float64)
-	wind := data["wind"].(map[string]interface{})["speed"].(float64)
-
 	sunriseTimestamp := time.Unix(int64(data["sys"].(map[string]interface{})["sunrise"].(float64)), 0)
 	sunsetTimestamp := time.Unix(int64(data["sys"].(map[string]interface{})["sunset"].(float64)), 0)
-	lengthOfDay := sunsetTimestamp.Sub(sunriseTimestamp)
+	weatherData := WeatherData{
+		City:        data["name"].(string),
+		Humidity:    weather["humidity"].(float64),
+		Pressure:    weather["pressure"].(float64),
+		WindSpeed:   data["wind"].(map[string]interface{})["speed"].(float64),
+		Sunrise:     time.Unix(int64(data["sys"].(map[string]interface{})["sunrise"].(float64)), 0),
+		Sunset:      time.Unix(int64(data["sys"].(map[string]interface{})["sunset"].(float64)), 0),
+		CurWeather:  weather["temp"].(float64),
+		wd:          "Посмотри в окно, не пойму что там за погода!",
+		LengthOfDay: sunsetTimestamp.Sub(sunriseTimestamp),
+	}
 
-	fmt.Printf("***%s***\nПогода в городе: %s\nТемпература: %.2fC° %s\nВлажность: %.0f%%\nДавление: %.0f мм.рт.ст\nВетер: %.2f м/с\nВосход солнца: %s\nЗакат солнца: %s\nПродолжительность дня: %s\nХорошего дня!",
-		time.Now().Format("2006-01-02 15:04"),
-		city,
-		curWeather,
-		wd,
-		humidity,
-		pressure,
-		wind,
-		sunriseTimestamp.Format("2006-01-02 15:04:05"),
-		sunsetTimestamp.Format("2006-01-02 15:04:05"),
-		lengthOfDay.String(),
-	)
+	// Возвращаем данные о погоде
+	return weatherData
+
 }
 
 var codeToSmile = map[string]string{
@@ -76,11 +82,6 @@ var codeToSmile = map[string]string{
 }
 
 func main() {
-	var city string
-
-	fmt.Print("Введите город: ")
-	fmt.Scanln(&city)
-	GetWeather(city, openWeatherMapAPI)
 
 	bot, err := tgbotapi.NewBotAPI("6537307160:AAEizhtTdKGu1ez5Jeb_uWjvfZg43GxTDaI")
 	if err != nil {
@@ -107,7 +108,24 @@ func main() {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Просто отправь мне название города и я покажу тебе погоду!")
 			bot.Send(msg)
 		} else {
+			city :=/////////////////////////////////////////////////////////////////////////////////////
+			weatherData := GetWeather(city, "e71cc7509ba7040322d574ebdad1b5c3")
 			go GetWeather(city, "e71cc7509ba7040322d574ebdad1b5c3")
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("***%s***\nПогода в городе: %s\nТемпература: %.2fC° %s\nВлажность: %.0f%%\nДавление: %.0f мм.рт.ст\nВетер: %.2f м/с\nВосход солнца: %s\nЗакат солнца: %s\nПродолжительность дня: %s\nХорошего дня!",
+				time.Now().Format("2006-01-02 15:04"),
+				city,
+				weatherData.CurWeather,
+				weatherData.wd,
+				weatherData.Humidity,
+				weatherData.Pressure,
+				weatherData.WindSpeed,
+				weatherData.Sunrise.Format("2006-01-02 15:04:05"),
+				weatherData.Sunset.Format("2006-01-02 15:04:05"),
+				weatherData.LengthOfDay.String(),
+			))
+			bot.Send(msg)
+
 		}
 	}
 }
